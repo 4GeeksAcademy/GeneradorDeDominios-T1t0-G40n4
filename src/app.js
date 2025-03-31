1221c5f5bf9f1e521b8c9c76d58be1dccf8c506c
T1t0-G40n4 <titogaona84@gmail.com>
2025-03-31 18:30:06 +0000

    RevisionLineamientosDeCodigo T1t0-G40n4

--- src/app.js
+++ src/app.js
@@ -5,30 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-  let pronoun = ['the', 'our'];
-  let adj = ['great', 'big'];
-  let noun = ['jogger', 'racoon'];
-  let dom = ['.com', '.net', '.us', '.io']
-
 
 window.onload = function() {
   //write your code here
+  let pronoun = ['the', 'our'];
+  let adjectif = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let domain = ['.com', '.net', '.us', '.io']
+  //para ver el resultado en la pagina sin tener que abrir la consola
+  let myDomainList = document.getElementById("generadorDeDominios");
+
  for (let i = 0; i < pronoun.length; i++) {
-    for (let j = 0; j < adj.length; j++) {
+    for (let j = 0; j < adjectif.length; j++) {
       for (let k = 0; k < noun.length; k++) {
-        for (let l = 0; l < dom.length; l++) {
-          let generadorDeDominios = pronoun[i] + adj[j]+ noun[k]+ dom[l]
-          console.log(pronoun[i] + adj[j]+ noun[k]+ dom[l] )
+        for (let l = 0; l < domain.length; l++) {
+          let generadorDeDominios = pronoun[i] + adjectif[j]+ noun[k]+ domain[l];
+          	//crear un <li> Element cada vez que hace el bucle
+          let item = document.createElement ("li");
+          item.textContent = generadorDeDominios;
+          myDomainList.appendChild(item);
+          console.log(generadorDeDominios);
          
         }
         
       }
       
-    }
-  
+    } 
+    console.log(generadorDeDominios);
  }
  
  console.log("Hello Rigo from the console!");
 
 }
-
